3b0d1258749dab66df93893b75b564a1554f52be
Alex Krestyannikov <avocodefreetrial@yandex.ru>
2023-09-24 21:37:21 +0500

    выполнил задание 22

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
 import pageScroll from './modules/pageScroll';
+import tabs from './modules/tabs';
 
 timer('19 september 2023');
 menu();
 modal();
 pageScroll();
+tabs();
