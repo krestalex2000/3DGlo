f5ce933c74dd0c04c2575403fc056325b3969056
Alex Krestyannikov <avocodefreetrial@yandex.ru>
2023-09-24 18:37:57 +0500

    выполнил задание 21

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
 import pageScroll from './modules/pageScroll';
+import calc from './modules/calc';
+import formValidate from './modules/formValidate';
 
 timer('19 september 2023');
 menu();
 modal();
 pageScroll();
+calc();
+formValidate();
